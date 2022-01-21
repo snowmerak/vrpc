54c9c3d1a9180765a2f1923d6e41717562337298
snowmerak <snowmerak@outlook.com>
2022-01-21 21:57:08 +0900

    add error branch

--- server.go
+++ server.go
@@ -44,43 +44,63 @@ func (s *Server) handler(conn net.Conn) {
 
 	s.conns[conn] = true
 	s.logger.Printf("%s: connected\n", conn.RemoteAddr())
-	header := [16]byte{}
-	n, err := conn.Read(header[:])
-	if err != nil {
-		s.logger.Printf("%s: cannot read header: %s\n", conn.RemoteAddr(), err)
-		return
-	}
-	if n != len(header) {
-		s.logger.Printf("%s: cannot read header: %s\n", conn.RemoteAddr(), "short read")
-		return
-	}
-	bodySize := uint64(frame.Frame(header[:]).BodySize())
+
+	bodySize := uint64(0)
 	curSize := uint64(0)
-	data.Write(header[:])
+	start := true
 	for {
+		if start {
+			header := [16]byte{}
+			n, err := conn.Read(header[:])
+			if err != nil {
+				s.logger.Printf("%s: cannot read header: %s\n", conn.RemoteAddr(), err)
+				return
+			}
+			if n != len(header) {
+				s.logger.Printf("%s: cannot read header: %s\n", conn.RemoteAddr(), "short read")
+				return
+			}
+			bodySize = uint64(frame.Frame(header[:]).BodySize())
+			curSize = 0
+			data.Write(header[:])
+			start = false
+		}
 		n, err := conn.Read(buf[:])
 		if err != nil {
 			s.logger.Printf("%s: error occurred: %s\n", conn.RemoteAddr(), err)
 			data.Reset()
+			start = true
 			continue
 		}
 		s.logger.Printf("%s: read %d bytes\n", conn.RemoteAddr(), n)
 		data.Write(buf[:n])
 		curSize += uint64(n)
 		if curSize >= bodySize {
+			if curSize > bodySize {
+				s.logger.Printf("%s: body size mismatch: %d > %d\n", conn.RemoteAddr(), curSize, bodySize)
+				data.Reset()
+				start = true
+				continue
+			}
 			frm := frame.Frame(data.Bytes())
 			if !frm.Vstruct_Validate() {
 				s.logger.Printf("%s: Invalid vstruct frame\n", conn.RemoteAddr())
 				return
 			}
 			replyBody := s.methods[frm.Service()][frm.Method()](frm.Body())
-			result := frame.New_Frame(frm.Service(), frm.Method(), frm.Sequence()+1, uint32(len(replyBody)), replyBody)
+			result := frame.New_Frame(frm.Service(), frm.Method(), frm.Sequence()+1, uint32(len(replyBody)+8), replyBody)
 			n, err := conn.Write(result)
 			if err != nil {
 				s.logger.Printf("%s: error writing to connection: %s\n", conn.RemoteAddr(), err)
 				return
 			}
+			if n != len(result) {
+				s.logger.Printf("%s: short write: %d != %d\n", conn.RemoteAddr(), n, len(result))
+				return
+			}
 			s.logger.Printf("%s: wrote %d bytes\n", conn.RemoteAddr(), n)
+			data.Reset()
+			start = true
 		}
 	}
 }
